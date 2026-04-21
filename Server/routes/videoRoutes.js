eaaac0bccb0b379ac8a361f234ed46bb91fd26b7
karthiksetty7 <137997670+karthiksetty7@users.noreply.github.com>
2026-04-21 18:39:08 +0530

    Add route to get video by ID

--- Server/routes/videoRoutes.js
+++ Server/routes/videoRoutes.js
@@ -1,8 +1,16 @@
 import express from 'express'
-import { getVideos } from '../controllers/videoController.js'
+import {getVideos, getVideoById} from '../controllers/videoController.js'
 
 const router = express.Router()
 
 router.get('/', getVideos)
+router.get('/:id', (req, res) => {
+  console.log("🔥 ROUTE WORKS:", req.params.id)
+
+  res.json({
+    success: true,
+    id: req.params.id,
+  })
+})
 
 export default router
