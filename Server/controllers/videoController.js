dc65645de960192528eefbae12d453c6136f4e99
karthiksetty7 <137997670+karthiksetty7@users.noreply.github.com>
2026-04-21 18:57:17 +0530

    Refactor video controller methods

--- Server/controllers/videoController.js
+++ Server/controllers/videoController.js
@@ -6,7 +6,7 @@ const escapeRegex = value =>
 // GET videos by category + optional search
 export const getVideos = async (req, res) => {
   try {
-    const {category = '', search = ''} = req.query
+    const { category = '', search = '' } = req.query
 
     const filter = {}
 
@@ -39,11 +39,7 @@ export const getVideos = async (req, res) => {
 // GET single video by custom id
 export const getVideoById = async (req, res) => {
   try {
-    console.log("🔥 REAL CONTROLLER HIT:", req.params.id)
-
     const video = await Video.findOne({ id: req.params.id })
-
-    console.log("🔥 VIDEO FOUND:", video)
 
     if (!video) {
       return res.status(404).json({
@@ -57,8 +53,6 @@ export const getVideoById = async (req, res) => {
       video,
     })
   } catch (error) {
-    console.error("🔥 ERROR:", error)
-
     return res.status(500).json({
       success: false,
       message: "Server error",
