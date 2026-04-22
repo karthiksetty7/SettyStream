dfb2f199d9f6afe119c7c052aae7b266b08b9dd8
karthiksetty7 <137997670+karthiksetty7@users.noreply.github.com>
2026-04-22 18:25:07 +0530

    Update handleAuthError to clear more auth data
    Enhanced handleAuthError to remove additional tokens and user information from localStorage.

--- Client/src/utils/auth.js
+++ Client/src/utils/auth.js
@@ -2,8 +2,17 @@ import Cookies from 'js-cookie'
 
 export const handleAuthError = history => {
   alert('Session expired. Please login again.')
-  localStorage.removeItem('token')
+
   Cookies.remove('token')
+  Cookies.remove('jwt_token')
+
+  localStorage.removeItem('token')
+  localStorage.removeItem('jwt_token')
+  localStorage.removeItem('user_id')
+  localStorage.removeItem('username')
+  localStorage.removeItem('user_name')
+
+  window.dispatchEvent(new Event('auth-change'))
 
   if (history && history.replace) {
     history.replace('/login')
