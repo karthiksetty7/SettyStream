f32f39ad3c0f0dd071a00fb5387a919e79cfb0a9
karthiksetty7 <137997670+karthiksetty7@users.noreply.github.com>
2026-04-22 17:34:09 +0530

    Add files via upload

---
+++ Server/models/DislikedVideo.js
@@ -0,0 +1,74 @@
+import mongoose from 'mongoose'
+
+const dislikedVideoSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    videoId: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    video: {
+      id: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      title: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      thumbnail_url: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      view_count: {
+        type: String,
+        default: '',
+        trim: true,
+      },
+      published_at: {
+        type: String,
+        default: '',
+        trim: true,
+      },
+      video_url: {
+        type: String,
+        default: '',
+        trim: true,
+      },
+      category: {
+        type: String,
+        default: '',
+        trim: true,
+      },
+      channel: {
+        name: {
+          type: String,
+          default: '',
+          trim: true,
+        },
+        profile_image_url: {
+          type: String,
+          default: '',
+          trim: true,
+        },
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+dislikedVideoSchema.index({userId: 1, videoId: 1}, {unique: true})
+
+const DislikedVideo = mongoose.model('DislikedVideo', dislikedVideoSchema)
+
+export default DislikedVideo
