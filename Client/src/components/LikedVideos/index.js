f19422946a6040290b4a8a96ae8a93d84a81de04
karthiksetty7 <137997670+karthiksetty7@users.noreply.github.com>
2026-04-22 16:40:13 +0530

    Refactor LikedVideos to use useContext
    Refactor LikedVideos component to use useContext for BackgroundContext.

--- Client/src/components/LikedVideos/index.js
+++ Client/src/components/LikedVideos/index.js
@@ -1,3 +1,4 @@
+import {useContext} from 'react'
 import {AiFillLike} from 'react-icons/ai'
 
 import BackgroundContext from '../../BackgroundContext'
@@ -7,48 +8,48 @@ import CommonVideosList from '../CommonVideosList'
 
 import '../../CommonVideosPage.css'
 
-const LikedVideos = () => (
-  <BackgroundContext.Consumer>
-    {({likedVideos, isDarkMode, removeLikedVideo}) => {
-      const pageClassName = `common-page ${isDarkMode ? 'common-page--dark' : ''}`
-      const hasLikedVideos = likedVideos.length > 0
+const LikedVideos = () => {
+  const {likedVideos, isDarkMode, removeLikedVideo} =
+    useContext(BackgroundContext)
 
-      return (
-        <>
-          <Header />
-          <div className="nav-sections-container">
-            <LeftNavBar />
-            <main className={pageClassName}>
-              <div className="common-page__header common-page__header--between">
-                <div className="common-page__title-wrap">
-                  <AiFillLike className="common-page__icon" />
-                  <h1>Liked Videos</h1>
-                </div>
-              </div>
+  const pageClassName = `common-page ${isDarkMode ? 'common-page--dark' : ''}`
+  const hasLikedVideos = likedVideos.length > 0
 
-              {hasLikedVideos ? (
-                <ul className="common-page__grid">
-                  {likedVideos.map(video => (
-                    <li key={video.id}>
-                      <CommonVideosList
-                        eachVideo={video}
-                        onDeleteVideo={removeLikedVideo}
-                        deleteLabel="Remove from Liked Videos"
-                      />
-                    </li>
-                  ))}
-                </ul>
-              ) : (
-                <div className="common-page__empty">
-                  <h2>No liked videos</h2>
-                </div>
-              )}
-            </main>
+  return (
+    <>
+      <Header />
+      <div className="nav-sections-container">
+        <LeftNavBar />
+        <main className={pageClassName}>
+          <div className="common-page__header common-page__header--between">
+            <div className="common-page__title-wrap">
+              <AiFillLike className="common-page__icon" />
+              <h1>Liked Videos</h1>
+            </div>
           </div>
-        </>
-      )
-    }}
-  </BackgroundContext.Consumer>
-)
+
+          {hasLikedVideos ? (
+            <ul className="common-page__grid">
+              {likedVideos.map(video => (
+                <li key={video.id}>
+                  <CommonVideosList
+                    eachVideo={video}
+                    onDeleteVideo={removeLikedVideo}
+                    deleteLabel="Remove from Liked Videos"
+                  />
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div className="common-page__empty">
+              <h2>No liked videos</h2>
+              <p>Videos you like will appear here</p>
+            </div>
+          )}
+        </main>
+      </div>
+    </>
+  )
+}
 
 export default LikedVideos
