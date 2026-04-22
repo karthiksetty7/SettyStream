b885bcf96c4f87c8b7edaacb0863433369b4d10a
karthiksetty7 <137997670+karthiksetty7@users.noreply.github.com>
2026-04-22 12:38:52 +0530

    Add files via upload

---
+++ Server/routes/historyRoutes.js
@@ -0,0 +1,18 @@
+// server/routes/historyRoutes.js
+import express from 'express'
+import {
+  getHistoryVideos,
+  addHistoryVideo,
+  removeHistoryVideo,
+  clearHistoryVideos,
+} from '../controllers/historyController.js'
+import { protect } from '../middlewares/authMiddleware.js'
+
+const router = express.Router()
+
+router.get('/', protect, getHistoryVideos)
+router.post('/', protect, addHistoryVideo)
+router.delete('/', protect, clearHistoryVideos)
+router.delete('/:videoId', protect, removeHistoryVideo)
+
+export default router
