5fc6589803e086fd367eaa5e48266839ea525bfe
karthiksetty7 <137997670+karthiksetty7@users.noreply.github.com>
2026-04-22 15:42:21 +0530

    Fix user ID reference in auth middleware

--- Server/middlewares/authMiddleware.js
+++ Server/middlewares/authMiddleware.js
@@ -1,4 +1,3 @@
-// server/middlewares/authMiddleware.js
 import jwt from 'jsonwebtoken'
 import User from '../models/User.js'
 
@@ -22,7 +21,7 @@ export const protect = async (req, res, next) => {
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-    req.user = await User.findById(decoded.userId).select('-password')
+    req.user = await User.findById(decoded.id).select('-password')
 
     if (!req.user) {
       return res.status(401).json({
