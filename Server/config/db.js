a697b49c64e81b82e0ab0bb720828dfcb6ddb6ff
karthiksetty7 <137997670+karthiksetty7@users.noreply.github.com>
2026-04-23 12:04:03 +0530

    Remove host information from MongoDB connection log

--- Server/config/db.js
+++ Server/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI)
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
+    console.log(`MongoDB Connected`)
   } catch (error) {
     console.error(`MongoDB connection error: ${error.message}`)
     process.exit(1)
